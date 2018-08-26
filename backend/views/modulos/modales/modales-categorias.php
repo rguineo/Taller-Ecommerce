ff105c2df9ee7c688bee928752db57289fbb4aaa
rguineo <rguineoa@gmail.com>
2018-08-26 14:18:57 -0300

    Modificacion JS cambiar caracteres

--- backend/views/modulos/modales/modales-categorias.php
+++ backend/views/modulos/modales/modales-categorias.php
@@ -18,7 +18,7 @@
           <div class="form-group row">
             <label class="col-sm-2 col-form-label">Ruta</label>
             <div class="col-sm-10">
-              <input type="text"id="ruta-categoria"  class="form-control" placeholder="Ruta" onkeyup=""  required name="rutaCategorias">
+              <input type="text"id="ruta-categoria"  class="form-control" placeholder="Ruta" onkeyup=""  required name="rutaCategorias" readonly>
             </div>
           </div>
 
