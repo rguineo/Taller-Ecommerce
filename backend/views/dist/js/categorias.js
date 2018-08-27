5d2ebd7684249266eb4efab55ebdd07864a71539
caceres_tm <caceres_tm@hotmail.com>
2018-08-27 01:44:08 -0300

    cambio en JS

--- backend/views/dist/js/categorias.js
+++ backend/views/dist/js/categorias.js
@@ -35,67 +35,10 @@ $("#titulo-categoria").on("keyup", function(){
     $("#ruta-categoria").val(cadena)
 })
 
-<<<<<<< HEAD
 $("#titulo-subcategoria").on("keyup", function(){
     var cadena = $("#titulo-subcategoria").val()
     cadena = getCleanedString(cadena)
     $("#ruta-subcategoria").val(cadena)
-=======
-$("#formu-editar-categoria").submit(function (e) {
-    e.preventDefault()
-
-    var datos = new FormData($(this)[0])
-
-    $.ajax({
-        url: 'ajax/ajaxCategorias.php',
-        type: 'POST',
-        data: datos,
-        processData: false,
-        contentType: false,
-        success: function(respuesta) {
-            if (respuesta == "ok") {
-                swal({
-                  type: 'success',
-                  title: 'Actualizado',
-                  text: 'Categoria actualizado con éxito'
-                }).then((result) => {
-                  if (result.value) {
-                    window.location = "slider"
-                  }
-                })
-            }
-        }
-
-    })
-})
-
-$("body .table-dark").on("click", ".btnEditarCategoria", function(){
-    var idCategoria = $(this).attr("idSlider")
-    var datos = new FormData()
-    datos.append("id", idCategoria)
-    datos.append("tipoOperacion", "editarCategoria")
-
-    $.ajax({
-        url: 'ajax/ajaxCategorias.php',
-        type: 'POST',
-        data: datos,
-        processData: false,
-        contentType: false,
-        success: function(respuesta) {
-            var valor = JSON.parse(respuesta)
-            console.log(valor.id)
-            console.log(valor.titulo_slider)
-
-            $('#formu-editar-categoria input[name="tituloCategoria"]').val(valor.categoria)
-            $('#formu-editar-categoria input[name="rutaCategoria"]').val(valor.ruta)
-            $('#formu-editar-categoria #imagenSlider').attr("src", valor.imagen)
-            $('#formu-editar-categoria input[name="id_slider"]').val(valor.idCategoria)
-
-        }
-
-    })
-
->>>>>>> 461dfc86ec594d4d34e4db365dabfd3d722763c9
 })
 
 function getCleanedString(cadena){
